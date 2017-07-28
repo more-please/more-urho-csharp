5ffc6b7ab736020b5f3b65db89db6e2a7677c5bb
Iain Merrick <iain@iainmerrick.com>
2017-07-28 14:25:22 -0500

    Another animation tweak: don't immediately finish animations which are overridden. Just remove Animated objects from them individually, as needed

---
+++ Collections.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Game.Extensions
+{
+	public static class Collections
+	{
+		public static bool IsEmpty<T>(this ICollection<T> collection)
+		{
+			return collection.Count == 0;
+		}
+	}
+}
