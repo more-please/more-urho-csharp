2fbdd151fd71af1a57a2eb54a47739797ca8b9a8
Iain Merrick <iain@iainmerrick.com>
2017-08-01 15:16:03 -0500

    Attach thoughts to links

--- Collections.cs
+++ Collections.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
 
 namespace Game.Extensions
 {
 	public static class Collections
 	{
-		public static bool IsEmpty<T>(this ICollection<T> collection)
+		public static bool IsEmpty(this ICollection collection)
 		{
 			return collection.Count == 0;
 		}
