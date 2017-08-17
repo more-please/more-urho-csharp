914825f266616b9cbd87b6468a2ca355dad48630
Iain Merrick <iain@iainmerrick.com>
2017-08-17 15:31:44 -0500

    Add UrhoObject (renaming to MoreUrho to avoid name clash with Urho.UrhoObject)

---
+++ Source/MoreUrho.cs
@@ -0,0 +1,13 @@
+﻿using Urho;
+using Urho.IO;
+using Urho.Resources;
+
+namespace More.Urho
+{
+	public abstract class MoreUrho
+	{
+		protected Log Log => Application.Current.Log;
+		protected ResourceCache Cache => Application.Current.ResourceCache;
+		protected Graphics Graphics => Application.Current.Graphics;
+	}
+}
